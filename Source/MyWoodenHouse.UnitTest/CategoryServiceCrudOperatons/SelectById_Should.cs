572104b0b68e0846b9a3dbc72b1292399a3755ef
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-08 19:07:37 +0200

    test project added

---
+++ Source/MyWoodenHouse.UnitTest/CategoryServiceCrudOperatons/SelectById_Should.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWoodenHouse.UnitTest.CategoryServiceCrudOperatons
+{
+    [TestClass]
+    public class SelectById_Should
+    {
+        [TestMethod]
+        public void ReturnCategory_WhenValidIdParameterPassed()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+
+    }
+}
