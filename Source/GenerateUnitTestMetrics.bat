e550b1641e50ff26d19d11ce6c111318405f6a44
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-28 19:26:37 +0300

    Working on Jenkins Coverage Report

--- Source/GenerateUnitTestMetrics.bat
+++ Source/GenerateUnitTestMetrics.bat
@@ -1,7 +1,21 @@
-"C:\Program Files (x86)\Jenkins\workspace\MyWoodenHouse-AutomaticUnitTesting\Source\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"  ^
+rem ============================
+rem Configure the UnitTests result file
+rem Configure the UnitTests dll files
+rem ============================
+
+@echo off 
+set "resultFile=\"All.UnitTest-Results.trx\""
+set "testContainer_1=\".\MyWoodenHouse.Data.Provider.UnitTests\bin\Release\MyWoodenHouse.Data.Provider.UnitTests.dll\""
+set "testContainer_2=\".\MyWoodenHouse.Data.Services.UnitTest\bin\Release\MyWoodenHouse.Data.Services.UnitTest.dll\""
+
+rem /testcontainer:%testContainer_2%
+rem /testcontainer:%testContainer_1%
+rem /resultsfile:%resultFile%
+
+".\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" ^
 -register:user ^
 -target:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" ^
--targetargs:"/testcontainer:\"C:\Program Files (x86)\Jenkins\workspace\MyWoodenHouse-AutomaticUnitTesting\Source\MyWoodenHouse.Data.Services.UnitTest\bin\Release\MyWoodenHouse.Data.Services.UnitTest.dll\" /resultsfile:\"ALL.UnitTest-Results.trx\"" ^
+-targetargs:" /testcontainer:%testContainer_1% /testcontainer:%testContainer_2% /resultsfile:%resultFile%" ^
 -filter:"+[*]* -[MyWoodenHouse.*Tests*]*" ^
 -mergebyhash ^
 -skipautoprops ^
