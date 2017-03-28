e54b52164a8e7623ddc6e722634559aca23396a3
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-28 22:19:52 +0300

    Working on Jenkins Coverage Repor

--- Source/GenerateReportGenerator-Output.bat
+++ Source/GenerateReportGenerator-Output.bat
@@ -2,7 +2,6 @@ rem ============================
 rem GenerateReportGenerator file
 rem ============================
 
-
 ".\Source\packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe" ^
 -reports:".\Source\GeneratedReports\MyWoodenHouse.OpenCover-Report.xml" ^
 -targetdir:".\Source\GeneratedReports\ReportGenerator Output"
