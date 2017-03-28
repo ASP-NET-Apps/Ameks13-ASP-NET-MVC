c83a60e674d17d0c405ca5e7a5359184bb696953
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-28 20:10:27 +0300

    no message

--- Source/GenerateReportGenerator-Output.bat
+++ Source/GenerateReportGenerator-Output.bat
@@ -2,6 +2,7 @@ rem ============================
 rem GenerateReportGenerator file
 rem ============================
 
+
 ".\Source\packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe" ^
 -reports:".\Source\GeneratedReports\MyWoodenHouse.OpenCover-Report.xml" ^
 -targetdir:".\Source\GeneratedReports\ReportGenerator Output"
