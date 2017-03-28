2341f8413340c24d34819b44ffa01bb7e8a25279
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-28 17:27:02 +0300

    Working on Jenkins Coverage Report

--- Source/GenerateUnitTestMetrics.bat
+++ Source/GenerateUnitTestMetrics.bat
@@ -1,8 +1,8 @@
 "C:\Program Files (x86)\Jenkins\workspace\MyWoodenHouse-AutomaticUnitTesting\Source\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe"  ^
 -register:user ^
 -target:"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\MSTest.exe" ^
--targetargs:"/testcontainer:  \"C:\Program Files (x86)\Jenkins\workspace\MyWoodenHouse-AutomaticUnitTesting\Source\MyWoodenHouse.Data.Provider.UnitTests\bin\Release\MyWoodenHouse.Data.Provider.UnitTests.dll\"  /resultsfile:\"ALL.RESULT.trx\""^
--filter:"+[*]*  -[MyWoodenHouse.*Tests*]*" ^
+-targetargs:"/testcontainer:\"C:\Program Files (x86)\Jenkins\workspace\MyWoodenHouse-AutomaticUnitTesting\Source\MyWoodenHouse.Data.Services.UnitTest\bin\Release\MyWoodenHouse.Data.Services.UnitTest.dll\" /resultsfile:\"ALL.UnitTest-Results.trx\"" ^
+-filter:"+[*]* -[MyWoodenHouse.*Tests*]*" ^
 -mergebyhash ^
 -skipautoprops ^
--output:"\GeneratedReports\MyWoodenHouse-Report.xml"
+-output:".\GeneratedReports\MyWoodenHouse.OpenCover-Report.xml"
