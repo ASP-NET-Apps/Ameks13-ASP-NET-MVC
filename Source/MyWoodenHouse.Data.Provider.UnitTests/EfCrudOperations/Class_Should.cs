ee88c5b8caa02fd58f04c0d31a3992b046a52beb
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-29 12:47:45 +0300

    New Unit Tests added for MyWoodenHouseDbContext - Class_Should EfCrudOperations tests added and reordered

---
+++ Source/MyWoodenHouse.Data.Provider.UnitTests/EfCrudOperations/Class_Should.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyWoodenHouse.Data.Provider.Contracts;
+using MyWoodenHouse.Data.Provider.Operations;
+using MyWoodenHouse.Ef.Models;
+using MyWoodenHouse.Extensions;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MyWoodenHouse.Data.Provider.UnitTest.EfCrudOperationsTests
+{
+    [TestClass]
+    public class Class_Should
+    {
+        private static Mock<IMyWoodenHouseDbContext> mockedMyWoodenHouseDbContext;
+        private static Mock<DbSet<Category>> mockedDbSet;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            mockedMyWoodenHouseDbContext = new Mock<IMyWoodenHouseDbContext>();
+            mockedDbSet = new Mock<DbSet<Category>>();
+
+            mockedMyWoodenHouseDbContext.Setup(mock => mock.Set<Category>()).Returns(mockedDbSet.Object);
+        }
+
+        [TestMethod]
+        public void HasProperty_Context()
+        {
+            // Arrange
+            // Done in the TestInit method
+
+            // Act
+            var actualInstance = new EfCrudOperatons<Category>(mockedMyWoodenHouseDbContext.Object);
+
+            // Assert
+            Assert.IsTrue(actualInstance.HasProperty("Context"));
+        }
+
+        [TestMethod]
+        public void HasPropertyOfCorrectType_Categories()
+        {
+            // Arrange
+            // Done in the TestInit method
+
+            // Act
+            var actualInstance = new EfCrudOperatons<Category>(mockedMyWoodenHouseDbContext.Object);
+
+            // Assert
+            Assert.IsInstanceOfType(actualInstance.Context, typeof(IMyWoodenHouseDbContext));
+        }
+        
+        [TestMethod]
+        public void HasProperty_DbSet()
+        {
+            // Arrange
+            // Done in the TestInit method
+
+            // Act
+            var actualInstance = new EfCrudOperatons<Category>(mockedMyWoodenHouseDbContext.Object);
+
+            // Assert
+            Assert.IsTrue(actualInstance.HasProperty("DbSet"));
+        }
+
+        [TestMethod]
+        public void HasPropertyOfCorrectType_DbSet()
+        {
+            // Arrange
+            // Done in the TestInit method
+
+            // Act
+            var actualInstance = new EfCrudOperatons<Category>(mockedMyWoodenHouseDbContext.Object);
+
+            // Assert
+            Assert.IsInstanceOfType(actualInstance.DbSet, typeof(IDbSet<Category>));
+        }
+
+        [TestMethod]
+        public void HasProperty_All()
+        {
+            // Arrange
+            // Done in the TestInit method
+
+            // Act
+            var actualInstance = new EfCrudOperatons<Category>(mockedMyWoodenHouseDbContext.Object);
+
+            // Assert
+            Assert.IsTrue(actualInstance.HasProperty("All"));
+        }
+
+        [TestMethod]
+        public void HasPropertyOfCorrectType_All()
+        {
+            // Arrange
+            // Done in the TestInit method
+
+            // Act
+            var actualInstance = new EfCrudOperatons<Category>(mockedMyWoodenHouseDbContext.Object);
+
+            // Assert
+            Assert.IsInstanceOfType(actualInstance.All, typeof(IQueryable<Category>));
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            mockedMyWoodenHouseDbContext = null;
+            mockedDbSet = null;
+        }
+    }
+}
---
+++ Source/MyWoodenHouse.Data.Provider.UnitTests/MyWoodenHouseDbContext/Class_Should.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWoodenHouse.Data.Provider.Contracts;
+using MyWoodenHouse.Ef.Models;
+using MyWoodenHouse.Extensions;
+using System.Data.Entity;
+
+namespace MyWoodenHouse.Data.Provider.UnitTests.MyWoodenHouseDbContextTests
+{
+    [TestClass]
+    public class Class_Should
+    {
+        [TestMethod]
+        public void ImplementInterface()
+        {
+            // Arrange & Act
+            MyWoodenHouseDbContext actualInstance = new MyWoodenHouseDbContext();
+            IMyWoodenHouseDbContext actualInstanceInterface = actualInstance as IMyWoodenHouseDbContext;
+
+            // Assert
+            Assert.IsNotNull(actualInstanceInterface);
+        }
+
+        [TestMethod]
+        public void HasProperty_Categories()
+        {
+            // Arrange & Act
+            MyWoodenHouseDbContext actualInstance = new MyWoodenHouseDbContext();
+
+            // Assert
+            Assert.IsTrue(actualInstance.HasProperty("Categories"));
+        }
+
+        [TestMethod]
+        public void HasPropertyOfCorrectType_Categories()
+        {
+            // Arrange & Act
+            MyWoodenHouseDbContext actualInstance = new MyWoodenHouseDbContext();
+
+            // Assert
+            Assert.IsInstanceOfType(actualInstance.Categories, typeof(IDbSet<Category>));
+        }
+    }
+}
