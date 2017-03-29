429e1ae31c06ed158c1f84cc7e71cded331d0f61
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-29 11:43:11 +0300

    Ne Unit Tests added for EfDbContextSaveChanges - Class_Should

---
+++ Source/MyWoodenHouse.Data.Provider.UnitTests/EfDbContextSaveChanges/Class_Should.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyWoodenHouse.Data.Provider.Contracts;
+using MyWoodenHouse.Data.Provider.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWoodenHouse.Data.Provider.UnitTests.EfDbContextSaveChangesTests
+{
+    [TestClass]
+    public class Class_Should
+    {
+        private static Mock<IMyWoodenHouseDbContext> mockedMyWoodenHouseDbContext;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            // Arrange
+            mockedMyWoodenHouseDbContext = new Mock<IMyWoodenHouseDbContext>();
+        }
+
+        [TestMethod]
+        public void ImplementInterface()
+        {
+            // Arrange
+            // Done in the TestInit method
+
+            // Act
+            EfDbContextSaveChanges actualInstance = new EfDbContextSaveChanges(mockedMyWoodenHouseDbContext.Object);
+            IEfDbContextSaveChanges actualInstanceInterface = actualInstance as IEfDbContextSaveChanges;
+
+            // Assert
+            Assert.IsNotNull(actualInstanceInterface);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            mockedMyWoodenHouseDbContext = null;
+        }
+    }
+}
