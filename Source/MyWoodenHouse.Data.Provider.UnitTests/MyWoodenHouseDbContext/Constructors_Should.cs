80116fed9db5f6dc5a51c19b010f662a6720b2a4
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-29 12:24:40 +0300

    Ne Unit Tests added for MyWoodenHouseDbContext - Constructors_Should

---
+++ Source/MyWoodenHouse.Data.Provider.UnitTests/MyWoodenHouseDbContext/Constructors_Should.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWoodenHouse.Extensions;
+
+namespace MyWoodenHouse.Data.Provider.UnitTests.MyWoodenHouseDbContextTests
+{
+    [TestClass]
+    public class Constructors_Should
+    {
+        [TestMethod]
+        public void BePublic()
+        {
+            // Arrange & Act
+            bool hasPublicConstructors = typeof(MyWoodenHouseDbContext).HasPublicConstructor();
+
+            // Assert
+            Assert.IsTrue(hasPublicConstructors);
+        }
+    }
+}
