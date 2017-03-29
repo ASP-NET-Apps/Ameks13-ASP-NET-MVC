285570cc00c0cedde1cad111951e1d0ad8ae738f
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-29 15:50:28 +0300

    New Unit Tests project for the Pure Models - MyWoodenHouse.Pure.Models.UnitTests New Unit Tests added for CategoryModel- Class_Should New Unit Tests added for CategoryModel- Constructors_Should Deleted unused empty files

---
+++ Source/MyWoodenHouse.Pure.Models.UnitTests/CategoryModel/Class_Should.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWoodenHouse.Contracts.PureModels;
+using MyWoodenHouse.Contracts;
+using MyWoodenHouse.Extensions;
+
+namespace MyWoodenHouse.Pure.Models.UnitTests.CategoryModelTests
+{
+    [TestClass]
+    public class Class_Should
+    {
+        [TestMethod]
+        public void ImplementInterface_ICategoryModel()
+        {
+            // Arrange & Act
+            CategoryModel actualInstance = new CategoryModel();
+            ICategoryModel actualInstanceInterface = actualInstance as ICategoryModel;
+
+            // Assert
+            Assert.IsNotNull(actualInstanceInterface);
+        }
+
+        [TestMethod]
+        public void ImplementInterface_IHasIntId()
+        {
+            // Arrange & Act
+            CategoryModel actualInstance = new CategoryModel();
+            IHasIntId actualInstanceInterface = actualInstance as IHasIntId;
+
+            // Assert
+            Assert.IsNotNull(actualInstanceInterface);
+        }
+
+        [TestMethod]
+        public void HasProperty_Id()
+        {
+            // Arrange & Act
+            CategoryModel actualInstance = new CategoryModel();
+
+            // Assert
+            Assert.IsTrue(actualInstance.HasProperty("Id"));
+        }
+
+        [TestMethod]
+        public void HasPropertyOfCorrectType_Id()
+        {
+            // Arrange & Act
+            CategoryModel actualInstance = new CategoryModel();
+
+            // Assert
+            Assert.IsInstanceOfType(actualInstance.Id, typeof(int));
+        }
+
+        [TestMethod]
+        public void HasProperty_Name()
+        {
+            // Arrange & Act
+            CategoryModel actualInstance = new CategoryModel();
+
+            // Assert
+            Assert.IsTrue(actualInstance.HasProperty("Name"));
+        }
+
+        [TestMethod]
+        public void HasPropertyOfCorrectType_Name()
+        {
+            // Arrange & Act
+            CategoryModel actualInstance = new CategoryModel();
+
+            // Assert
+            Assert.IsInstanceOfType(actualInstance.Name, typeof(string));
+        }
+    }
+}
---
+++ Source/MyWoodenHouse.Pure.Models.UnitTests/CategoryModel/Constructors_Should.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWoodenHouse.Extensions;
+using MyWoodenHouse.Pure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWoodenHouse.Pure.Models.UnitTests.CategoryModelTests
+{
+    [TestClass]
+    public class Constructors_Should
+    {
+        [TestMethod]
+        public void BePublic()
+        {
+            // Arrange & Act
+            bool hasPublicConstructors = typeof(CategoryModel).HasPublicConstructor();
+
+            // Assert
+            Assert.IsTrue(hasPublicConstructors);
+        }
+
+        [TestMethod]
+        public void HaveEmptyConstructorAndReturnCorrectInstance_CategoryModel()
+        {
+            // Arrange & Act
+            var actualInstance = new CategoryModel();
+
+            // Assert
+            Assert.IsInstanceOfType(actualInstance, typeof(CategoryModel));
+        }
+    }
+}
