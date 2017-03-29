30b1c0897a69cdcd55e8e6304e969aad4e599a20
MikeSteiner <mike.steiner.77@gmail.com>
2017-03-29 13:41:28 +0300

    Unit Tests added for MyWoodenHouseDbContext - GetEntityState_Should Unit Tests added for MyWoodenHouseDbContext - SetEntityState_Should

---
+++ Source/MyWoodenHouse.Data.Provider.UnitTests/MyWoodenHouseDbContext/GetEntityState_Should.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyWoodenHouse.Data.Provider.Contracts;
+using MyWoodenHouse.Ef.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWoodenHouse.Data.Provider.UnitTests.MyWoodenHouseDbContextTests
+{
+    [TestClass]
+    public class GetEntityState_Should
+    {
+        [TestMethod]
+        public void ExistsAndCanBeCalled()
+        {
+            // Arrange & Act
+            Mock<IMyWoodenHouseDbContext> mockedMyWoodenHouseDbContext = new Mock<IMyWoodenHouseDbContext>();
+            var actualEntityState = mockedMyWoodenHouseDbContext.Object.GetEntityState(new Category());
+            
+            // Assert
+            mockedMyWoodenHouseDbContext.Verify(m => m.GetEntityState(It.IsAny<object>()), Times.Once);
+        }
+
+        [TestMethod]
+        public void ReturnsCorrectInstance_EntityState()
+        {
+            // Arrange & Act
+            Mock<IMyWoodenHouseDbContext> mockedMyWoodenHouseDbContext = new Mock<IMyWoodenHouseDbContext>();
+            var actualEntityState = mockedMyWoodenHouseDbContext.Object.GetEntityState(new Category());
+
+            // Assert
+            Assert.IsInstanceOfType(actualEntityState, typeof(EntityState));
+        }
+    }
+}
---
+++ Source/MyWoodenHouse.Data.Provider.UnitTests/MyWoodenHouseDbContext/SetEntityState_Should.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MyWoodenHouse.Data.Provider.Contracts;
+using MyWoodenHouse.Ef.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWoodenHouse.Data.Provider.UnitTests.MyWoodenHouseDbContextTests
+{
+    [TestClass]
+    public class SetEntityState_Should
+    {
+        [TestMethod]
+        public void ExistsAndCanBeCalled()
+        {
+            // Arrange & Act
+            Mock<IMyWoodenHouseDbContext> mockedMyWoodenHouseDbContext = new Mock<IMyWoodenHouseDbContext>();
+            mockedMyWoodenHouseDbContext.Object.SetEntityState(new Category(), EntityState.Modified);
+
+            // Assert
+            mockedMyWoodenHouseDbContext.Verify(m => m.SetEntityState(It.IsAny<object>(), It.IsAny<EntityState>()), Times.Once);
+        }
+    }
+}
