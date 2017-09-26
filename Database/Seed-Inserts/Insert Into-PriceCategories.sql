1119fccec7cbe7a1790dfeaf79db85f476cbfabc
alex.toplijski <alex.toplijski@gmail.com>
2017-09-26 13:13:30 +0300

    Database init script changed

--- Database/Seed-Inserts/Insert Into-PriceCategories.sql
+++ Database/Seed-Inserts/Insert Into-PriceCategories.sql
@@ -1,9 +1,9 @@
 USE [MyWoodenHouse]
 GO
 
-INSERT INTO [dbo].[PriceCategory] ([Id] ,[Name])
+INSERT INTO [dbo].[PriceCategories] ([Id] ,[Name])
      VALUES (1 ,'Family')	 
-INSERT INTO [dbo].[PriceCategory] ([Id] ,[Name])
+INSERT INTO [dbo].[PriceCategories] ([Id] ,[Name])
      VALUES (2 ,'Premium')
 GO
 
