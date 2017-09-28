a7e26ee591798bce9119271205f49902123397a8
alex.toplijski <alex.toplijski@gmail.com>
2017-09-28 18:38:01 +0300

    BaseGeneral Service interface created for the main CRUD operations

---
+++ Source/MyWoodenHouse.Data.Services/Contracts/IBaseGenericService.cs
@@ -0,0 +1,21 @@
+﻿using MyWoodenHouse.Contracts.Models;
+using MyWoodenHouse.Pure.Models;
+using System.Collections.Generic;
+
+namespace MyWoodenHouse.Data.Services.Contracts
+{
+    public interface IBaseGenericService<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(int? id);
+
+        int Insert(T entity);
+
+        T Update(T entity);
+
+        void Delete(int? id);
+
+        void Delete(T entity);
+    }
+}
---
+++ Source/MyWoodenHouse.Data.Services/MaterialService.cs
@@ -0,0 +1,44 @@
+﻿using MyWoodenHouse.Data.Services.Contracts;
+using MyWoodenHouse.Ef.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWoodenHouse.Data.Services
+{
+    public class MaterialService : IBaseGenericService<Material>
+    {
+
+        public IEnumerable<Material> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Material entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material Update(Material entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Material entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
