56ccdfd1deb73ea768f102d45826640b1673083a
alex.toplijski <alex.toplijski@gmail.com>
2017-10-03 18:02:52 +0300

    DbContext Entity to Table binding oprions

--- Source/MyWoodenHouse.Data.Provider/MyWoodenHouseDbContext.cs
+++ Source/MyWoodenHouse.Data.Provider/MyWoodenHouseDbContext.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System;
 using MyWoodenHouse.Contracts.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace MyWoodenHouse.Data.Provider
 {
@@ -55,22 +56,63 @@ namespace MyWoodenHouse.Data.Provider
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
             //Entity Framework CTP 4. “Cannot insert the value NULL into column” -Even though there is no NULL value
             //http://stackoverflow.com/questions/4444407/entity-framework-ctp-4-cannot-insert-the-value-null-into-column-even-though/5338384#5338384
             //Have you tried explicitly specifying the StoreGeneratedPattern?
             // modelBuilder.Entity<BOB>().HasKey(p => p.Id).Property(p => p.Id).HasDatabaseGenerationOption(DatabaseGenerationOption.None); 
             // modelBuilder.Entity<BOB>().ToTable("BOB");
+
+            //Building
+            modelBuilder.Entity<Building>()
+                .HasKey(c => c.Id)
+                .Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            modelBuilder.Entity<Building>().ToTable("Building");
+
+            //Categories
             modelBuilder.Entity<Category>()
                 .HasKey(c => c.Id)
                 .Property(p => p.Id)
                 .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
             modelBuilder.Entity<Category>().ToTable("Categories");
 
+            //Materials
             modelBuilder.Entity<Material>()
                 .HasKey(c => c.Id)
                 .Property(p => p.Id)
                 .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
             modelBuilder.Entity<Material>().ToTable("Materials");
+
+            //Pictures
+            modelBuilder.Entity<Picture>()
+                .HasKey(c => c.Id)
+                .Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            modelBuilder.Entity<Picture>().ToTable("Pictures");
+
+            //Prices
+            modelBuilder.Entity<Price>()
+                .HasKey(c => c.Id)
+                .Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            modelBuilder.Entity<Price>().ToTable("Prices");
+
+            //PriceCategories
+            modelBuilder.Entity<PriceCategory>()
+                .HasKey(c => c.Id)
+                .Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            modelBuilder.Entity<PriceCategory>().ToTable("PriceCategories");
+
+            //Products
+            modelBuilder.Entity<Product>()
+                .HasKey(c => c.Id)
+                .Property(p => p.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
+            modelBuilder.Entity<Product>().ToTable("Products");
 
             base.OnModelCreating(modelBuilder);
         }
